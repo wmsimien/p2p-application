e2ce25b204ce52db1b1847d159d3d7ae58743478
wmavery <wanda.avery@att.net>
2023-06-01 16:48:46 -0500

    update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'p2p-client';
+  title = 'Procure2Pay - Mini Application';
 }
