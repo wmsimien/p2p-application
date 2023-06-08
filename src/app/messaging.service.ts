5a0e01162793fdfa6278b00efb7b971c440c8311
wmavery <wanda.avery@att.net>
2023-06-08 00:19:19 -0500

    code refactoring and clean up

---
+++ src/app/messaging.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagingService {
+
+
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  /**
+   * Create a service for displaying snack-bar notifications using MatSnackBar.  Message will be open/dislay for a second toward the top view.
+   * @param message  Opens snackbar with provided message.
+   * @param action   Action to perform with message.
+   */
+  openSnackBar(message: string, action: string='ok') {
+    this.snackBar.open(message, action, {
+      duration: 1000,
+      verticalPosition: 'top'
+    });
+  }
+}
--- src/app/purchase-order-requisition-form/purchase-order-requisition-form.component.ts
+++ src/app/purchase-order-requisition-form/purchase-order-requisition-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { ApiUrlsService } from '../api-urls.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MessagingService } from '../messaging.service';
 
 @Component({
   selector: 'app-purchase-order-requisition-form',
@@ -27,6 +28,7 @@ picker: any;
 
   constructor(private fb: FormBuilder, 
     private apiUrls: ApiUrlsService, 
+    private messageService: MessagingService,
       private poReqsDialogRef: MatDialogRef<PurchaseOrderRequisitionFormComponent>,
       @Inject(MAT_DIALOG_DATA) public data: any, 
       ) {
@@ -55,20 +57,7 @@ picker: any;
      });
 
   }
-  getItems() : FormArray {
-    const itemControl = this.poReqsForm.get('itemId') as FormArray;
-    return itemControl;
-  }
 
-  newItems() : FormGroup{
-    return this.fb.group({
-      itemId: '',
-      itemName: '',
-      itemDescription: '',
-      qty:'',
-      uom:''
-    })
-  }
 
   /**
    * Method will perform update of an existing purchase requisition or create one.
@@ -80,7 +69,7 @@ picker: any;
         console.log(this.poReqsForm.value);
         this.apiUrls.updatePOReqById(this.data.id, this.poReqsForm.value).subscribe({
           next: (val: any) => {
-            alert('PO-Reqs Header update!');
+            this.messageService.openSnackBar('PO-Reqs Header update!');
             this.apiUrls.updatePOReqDetail(this.data.id, this.poReqsForm.value).subscribe({
               next: (val: any) => {
                 console.log(val);
@@ -98,9 +87,7 @@ picker: any;
       } else {
         this.apiUrls.createPOReqHeader(this.poReqsForm.value).subscribe({
           next: (val: any) => {
-            alert('PO-Reqs added successfully.');
-            console.log(this.data);
-            console.log(this.poReqsForm.value);
+            this.messageService.openSnackBar('PO-Reqs added successfully.');
             this.apiUrls.createPOReqDetail(this.data.id, this.poReqsForm.value).subscribe({
               next: (val: any) => {
                 this.poReqsDialogRef.close(true);
@@ -120,19 +107,6 @@ picker: any;
 
   ngOnInit(): void {
       this.poReqsForm.patchValue(this.data);
-      this.getItems();
-      const itemControl = this.poReqsForm.get('itemId') as FormArray;
-      
-      // this.poReqDetailList.forEach((element: any) => {
-      //   console.log(element);
-        
-      // });
-
-      // this.data.poReqDetailList.forEach(r => {
-      //   r.items.forEach(s => {
-      //     itemControl.push(this.fb.control(s.id));
-      //   });
-      // });
 
   }
   
